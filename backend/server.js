30e481c2e5c1eb80760e66efc12a0d4b52354e64
Connor Emery <Connorbemery@gmail.com>
2025-05-12 16:05:48 -0400

    oopsies

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config(); 
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const booksRoute = require('./routes/books');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api/books', booksRoute);
+
+// connect to MongoDB using .env variable
+mongoose.connect("npprocess.env.MONGODB_URI", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
